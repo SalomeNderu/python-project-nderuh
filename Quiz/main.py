2002ff88cbc131195c72737b22b7a58806a72a75
eMobilis <nathnaelsherry@outlook.com>
2023-02-14 11:56:14 +0300

    added a for loop

---
+++ Quiz/main.py
@@ -0,0 +1,14 @@
+#for loop that traverses numbers from 1 to 100
+for x in range(1,101):
+  #check if number is divisible by both 3 and 5
+  if(x%3==0 and x%5==0):
+    print("FizzBuzz")
+  #check if number is divisible by 3
+  elif(x%3 == 0):
+    print("Fizz")
+  #check if number is divisible by 5
+  elif(x%5 == 0):
+    print("Buzz")
+  #if not divisible by either of them print the x
+  else:
+    print(x)
