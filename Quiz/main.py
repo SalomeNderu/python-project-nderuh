4f6b7391da8d595dd8e16c4d0bc5cfe3b2949acf
SalomeNderu <sallienderu@gmail.com>
2023-02-14 12:28:29 +0300

    added a for loop

--- Quiz/main.py
+++ Quiz/main.py
@@ -2,13 +2,13 @@
 for x in range(1,101):
   #check if number is divisible by both 3 and 5
   if(x%3==0 and x%5==0):
-    print("FizzBuzz")
+    print(x,"FizzBuzz")
   #check if number is divisible by 3
   elif(x%3 == 0):
-    print("Fizz")
+    print(x,"Fizz")
   #check if number is divisible by 5
   elif(x%5 == 0):
-    print("Buzz")
+    print(x,"Buzz")
   #if not divisible by either of them print the x
   else:
     print(x)
