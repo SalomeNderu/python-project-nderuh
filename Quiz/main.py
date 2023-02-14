f85c2884dbdea5be2894932bb03d6d973abca2d9
SalomeNderu <sallienderu@gmail.com>
2023-02-14 12:53:12 +0300

    updated for loop

--- Quiz/main.py
+++ Quiz/main.py
@@ -1,5 +1,7 @@
+num1 = int(input("Enter first number"))
+num2 = int(input("Enter second number"))
 #for loop that traverses numbers from 1 to 100
-for x in range(1,101):
+for x in range(num1,num2):
   #check if number is divisible by both 3 and 5
   if(x%3==0 and x%5==0):
     print(x,"FizzBuzz")
